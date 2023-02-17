a7743884c4ba456982444230d100995497d9232b
username <ayspina@gmail.com>
2023-02-17 09:55:46 -0500

    Adds additional styling

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
+import './AuthPage.css';
 import LoginForm from "../../components/LoginForm/LoginForm"
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
 
 export default function AuthPage({ setUser }) {
+    const [showLogin, setShowLogin] = useState(true);
+
     return (
-        <main>
-            <h1>Auth Page</h1>
-            <SignUpForm setUser={setUser}/>
-            <LoginForm setUser={setUser} />
-        </main>
-        
-    )
-}
+      <main className="AuthPage">
+        <div>
+          <img src="https://i.imgur.com/a6LPjie.png" alt="" className='logo-img' />
+          <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+        </div>
+        {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+      </main>
+    );
+  }
--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,8 +1,14 @@
+import './OrderHistoryPage.css';
+
 export default function OrderHistoryPage() {
 
     return (
-        <>
-            <h1>Order History Page</h1>
-        </>
+        <div className="OrderHistoryPage">
+            <h1>Thank you for ordering!</h1>
+            <div className="cheers">
+                <img src="https://images.desenio.com/zoom/16969_2.jpg" alt="" className="cheers-img" />
+            </div>
+            
+        </div>
     )
 }
